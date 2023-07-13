24ffb7171ac7a217565719bebe610d5b67ac9c01
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-07-13 10:22:59 -0600

    feat: Iteration 1 methods and test

--- lib/player.rb
+++ lib/player.rb
@@ -1,3 +1,23 @@
-class Player
+class Player 
+  attr_reader :first_name,
+              :last_name,
+              :monthly_cost,
+              :contract_length
   
+  attr_accessor :nickname
+
+  def initialize(full_name, monthly_cost, contract_length)
+    @first_name, @last_name = full_name.split(" ")
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+    @nickname = nil
+  end
+
+  def total_cost
+    @monthly_cost * @contract_length
+  end
+
+  def set_nickname!(player_nickname)
+      @nickname = player_nickname
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -37,4 +37,17 @@ RSpec.describe Player do
 
     expect(player.total_cost).to eq(36000000)
   end
+
+  it "has a nickname" do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    expect(player.nickname).to eq(nil)
+  end
+
+  it "needs a nickname set" do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    player.set_nickname!("Squints")
+    expect(player.nickname).to eq("Squints")
+  end
 end
